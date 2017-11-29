f27f694fc410e70342c246f75bcfd9df0e5f0b77
STEVEN TRAN <StevenTran@dhcp-v081-085.mobile.uci.edu>
2017-11-28 19:14:29 -0800

    JS added

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,19 @@
-    $(document).ready(function(){
+    //Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyBf63vOsiOcfJbUYOzGNNSO85ZGf9MyzJc",
+      authDomain: "uci-bootcamp.firebaseapp.com",
+      databaseURL: "https://uci-bootcamp.firebaseio.com",
+      projectId: "uci-bootcamp",
+      storageBucket: "uci-bootcamp.appspot.com",
+      messagingSenderId: "290327709584"
+    };
 
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+
+    $(document).ready(function(){
 
       $('.btn').on('click', function(){
         // const client = filestack.init('ACvWqWhqT0uESSK94Rojtz');
@@ -16,7 +30,7 @@
 
             var img = response.filesUploaded[0].url
             var zipCode = $('#zipcode').val()
-            var name = $('#name').val()
+            var personName = $('#name').val()
 
             // Ajax call for Face++
             $.ajax({
@@ -51,59 +65,88 @@
                   function randomBrew(){
                     random = brewery[Math.floor(Math.random() * brewery.length)];
                     console.log(random)
-
-                    var name = random.brewery.name
+                    
+                    var breweryName = random.brewery.name
                     var address = random.streetAddress
                     var city = random.locality
                     var phone = random.phone
                     var website = random.brewery.website
                     var img = random.brewery.images.medium
 
+                      
+                    // Pushing to database
+                    var breweryData = {
+                      breweryName: breweryName,
+                      address: address,
+                      city: city,
+                      phone: phone,
+                      website: website,
+                      img: img,
+                    };
+
+                    database.ref().push(breweryData)
+
                     var newBrew = {
                       'card': [{
-                        'group': ['class1', 'class2'],
                         'image-url': img,
-                        'card_title': name,
+                        'card_title': breweryName,
                         'card_subtitle': 'Address: '+address+'<br>City: '+city+'<br>Phone: '+phone+'<br>Website: '+website,
-                        'badge': ['hot','win'],
                       }]
                     }
                     $.each(newBrew.card, function (key,data){
-                        console.log(data);
-                        $('#results').append('<div class="content col s12 m4 l4" >'+'<div class="card"><div class="card-image"><img src="' + data['image-url'] + '"></div>'+'<div class="card-content">'+'<span class="card-title grey-text text-darken-4">' + data.card_title + '</span>'+'<p class="card-subtitle grey-text text-darken-2">' + data.card_subtitle + '</p>'+'</div></div></div>');
-                      }) 
-                  }
+                      console.log(data);
+                      $('.current-match').append('<div class="content col s12 m6 l6" >'+'<div class="card"><div class="card-image"><img src="' + data['image-url'] + '"></div>'+'<div class="card-content">'+'<span class="card-title grey-text text-darken-4">' + data.card_title + '</span>'+'<p class="card-subtitle grey-text text-darken-2">' + data.card_subtitle + '</p>'+'</div></div></div>');
+                    }) 
+                     
+                    }
 
                     var person = {
                       'card': [{
-                        'group': ['class1', 'class2'],
                         'image-url': img,
-                        'card_title': name,
-                        'card_subtitle': 'Age: '+age+'<br>Happy: '+happy+'%'+'<br>Anger: '+anger+'%'+'<br>Sad: '+sad+'%',
-                        "badge": ["hot", "win"],         
+                        'card_title': personName,
+                        'card_subtitle': 'Age: '+age+'<br>Happy: '+happy+'%'+'<br>Anger: '+anger+'%'+'<br>Sad: '+sad+'%',         
                       }]
                     };
 
                     $.each(person.card, function (key,data){
                       console.log(data);
-                      $('#results').append('<div class="content col s12 m4 l4" >'+'<div class="card"><div class="card-image"><img src="' + data['image-url'] + '"></div>'+'<div class="card-content">'+'<span class="card-title grey-text text-darken-4">' + data.card_title + '</span>'+'<p class="card-subtitle grey-text text-darken-2">' + data.card_subtitle + '</p>'+'</div></div></div>');
+                      $('.current-match').append('<div class="content col s12 m6 l6" >'+'<div class="card"><div class="card-image"><img src="' + data['image-url'] + '"></div>'+'<div class="card-content">'+'<span class="card-title grey-text text-darken-4">' + data.card_title + '</span>'+'<p class="card-subtitle grey-text text-darken-2">' + data.card_subtitle + '</p>'+'</div></div></div>');
                     })
 
-                  randomBrew()
-                })
+                    randomBrew()
+                  })
               })
             })
-          })
+  })
   }
   openPicker();        
 });
+    // Pull from database and displaying on website 
+    database.ref().on('child_added', function(childSnapshot, prevChildKey){
+
+      console.log(childSnapshot.val());
+
+      var breweryName = childSnapshot.val().breweryName
+      var address = childSnapshot.val().address
+      var city = childSnapshot.val().city
+      var phone = childSnapshot.val().phone
+      var website = childSnapshot.val().website
+      var img = childSnapshot.val().img
+
+      var newBrew = {
+        'card': [{
+          'image-url': img,
+          'card_title': breweryName,
+          'card_subtitle': 'Address: '+address+'<br>City: '+city+'<br>Phone: '+phone+'<br>Website: '+website,          
+        }]
+      }
+      $.each(newBrew.card, function (key,data){
+        console.log(data);
+        $('.prev_match').append('<div class="content col s12 m3 l3" >'+'<div class="card area"><div class="card-image"><img src="' + data['image-url'] + '"></div>'+'<div class="card-content">'+'<span class="card-title grey-text text-darken-4">' + data.card_title + '</span>'+'<p class="card-subtitle grey-text text-darken-2">' + data.card_subtitle + '</p>'+'</div></div></div>');
+      }) 
+
+
+
+
+    });
   });
-
-
-
-
-
-
-
-
-
